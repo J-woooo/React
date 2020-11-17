6d53c9c07fb41270af04ae8d15b7aa35470ad658
Bae Jin Woo <jw969696@naver.com>
2020-11-18 02:25:51 +0900

    Unit9

---
+++ 9/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import StyledComponent from './StyledComponent';
+function App() {
+  return (
+    <div>
+      <StyledComponent />
+    </div>
+  );
+}
+
+export default App;
