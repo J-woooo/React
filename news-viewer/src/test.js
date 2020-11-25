13edd2b0325016b105825f90807c6def3313be52
Bae Jin Woo <jw969696@naver.com>
2020-11-25 23:28:13 +0900

    Unit14

---
+++ news-viewer/src/test.js
@@ -0,0 +1,35 @@
+function increase(number) {
+  const promise = new Promise((resolve, reject) => {
+    // resolve는 성공, reject는 실패
+    setTimeout(() => {
+      const result = number + 10;
+      if (result > 50) {
+        const e = new Error("NumberTooBig");
+        return reject(e);
+      }
+      resolve(result); //number 값에 + 10 후 성공 처리
+    }, 1000);
+  });
+  return promise;
+}
+
+async function runTasks() {
+  try {
+    let result = await increase(0);
+    console.log(result);
+    result = await increase(result);
+    console.log(result);
+    result = await increase(result);
+    console.log(result);
+    result = await increase(result);
+    console.log(result);
+    result = await increase(result);
+    console.log(result);
+    result = await increase(result);
+    console.log(result);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+runTasks(0);
